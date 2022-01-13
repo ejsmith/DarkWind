cf5ff1620a39b82e1646b5d46f4ab4a18dc9c272
Jason Alexander <jason.alexander@gmail.com>
2022-01-12 18:41:14 -0600

    Added SPs, slight refactor

---
+++ src/DarkWind.Client/Pages/Index.razor.cs
@@ -0,0 +1,105 @@
+﻿using System.Text.Json;
+using DarkWind.Client.Hubs;
+using DarkWind.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using XtermBlazor;
+
+namespace DarkWind.Client.Pages;
+
+public partial class Index 
+{
+    int health = 0;
+    int maxHealth = 0;
+    int spellpoints = 0;
+    int maxSpellpoints = 0;
+
+
+    private Xterm? _terminal;
+    private Xterm? _gmcp;
+    private string _input = String.Empty;
+    private CancellationTokenSource cts = new CancellationTokenSource();
+
+    [Inject] TelnetHub Hub { get; set; }
+    [Inject] IJSRuntime Javascript { get; set; }
+
+    private Task HandleGmcpMessage(string command, string data) {
+        switch (command) {
+            case "Char.Vitals":
+                var vitals = JsonSerializer.Deserialize<CharVitals>(data);
+                if (vitals == null) return Task.CompletedTask;
+
+                health = vitals.Health;
+                maxHealth = vitals.MaxHealth;
+                spellpoints = vitals.Spellpoints;
+                maxSpellpoints = vitals.MaxSpellpoints;
+
+                StateHasChanged();
+
+                break;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private async Task SendCommand(string command) {
+        await Hub.Send(command + '\r');
+        await _terminal!.Write(command + "\r\n");
+        await Javascript.InvokeVoidAsync("selectText", "input");
+    }
+
+    private async Task SendGmcpCommand(string command) {
+        if (String.IsNullOrEmpty(command)) return;
+
+        await Hub.SendGmcp(command);
+    }
+
+    private Task SendCommand() {
+        if (String.IsNullOrEmpty(_input)) return Task.CompletedTask;
+
+        return SendCommand(_input);
+    }
+
+
+    private async Task OnFirstRender() {
+        await Javascript.InvokeVoidAsync("selectText", "input");
+
+        await Hub.Start();
+
+        var channel = await Hub.Connect(cts.Token);
+
+        while (await channel.WaitToReadAsync()) {
+            while (channel.TryRead(out var message)) {
+                if (message.Option == TelnetMessage.KnownOptions.Echo) {
+                    await _terminal!.Write(message.Data);
+                }
+                else if (message.Option == TelnetMessage.KnownOptions.GMCP) {
+                    if (String.IsNullOrEmpty(message.Data)) continue;
+
+                    await _gmcp!.WriteLine(message.Data);
+
+                    var firstSpace = message.Data.IndexOf(' ');
+                    var command = firstSpace > 0 ? message.Data.Substring(0, firstSpace) : message.Data;
+                    var gmcpData = firstSpace > 0 && message.Data.Length > firstSpace ? message.Data.Substring(firstSpace) : String.Empty;
+
+                    try {
+                        await HandleGmcpMessage(command, gmcpData);
+                    }
+                    catch (Exception ex) {
+                        await _gmcp!.WriteLine("ERR: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+
+    private TerminalOptions _options = new TerminalOptions {
+        Rows = 30,
+        CursorStyle = CursorStyle.Bar
+    };
+
+    private TerminalOptions _gmcpOptions = new TerminalOptions {
+        Rows = 10,
+        CursorStyle = CursorStyle.Bar
+    };
+}
--- src/DarkWind.Shared/CharVitals.cs
+++ src/DarkWind.Shared/CharVitals.cs
@@ -2,9 +2,19 @@
 
 namespace DarkWind.Shared;
 
-public class CharVitals {
+public class CharVitals 
+{
+
     [JsonPropertyName("hp")]
     public int Health { get; set; }
+
     [JsonPropertyName("maxhp")]
     public int MaxHealth { get; set; }
+
+    [JsonPropertyName("sp")]
+    public int Spellpoints { get; set; }
+
+    [JsonPropertyName("maxsp")]
+    public int MaxSpellpoints { get; set; }
+
 }
